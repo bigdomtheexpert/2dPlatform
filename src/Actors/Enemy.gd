f2cc090a18aa7e873f2741c70ca6f72555edfd70
bigdomtheexpert <domenico.frigido@gmail.com>
2020-11-11 18:11:11 +0000

    add enemy

---
+++ src/Actors/Enemy.gd
@@ -0,0 +1,10 @@
+extends "res://src/Actors/Actors.gd"
+
+func _ready() -> void:
+	_velocity.x = -speed.x
+
+func _physics_process(delta: float) -> void:
+	if is_on_wall():
+		_velocity.x *= -1.0
+	_velocity = move_and_slide(_velocity,
+	 FLOOR_NORMAL)
