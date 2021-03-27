d1d081f4b66ebed543095f594b5943ce80f3d14c
bigdomtheexpert <domenico.frigido@gmail.com>
2021-03-27 19:43:45 +0000

    ok

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -1,10 +1,10 @@
 extends "res://src/Actors/Actors.gd"
 
-func _ready() -> void:
+func _ready() -> void: 
 	_velocity.x = -speed.x
 
 func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
 	if is_on_wall():
 		_velocity.x *= -1.0
-	_velocity = move_and_slide(_velocity,
-	 FLOOR_NORMAL)
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
